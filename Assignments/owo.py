b62cc0bec12e2882f13f836f7b7ca5511031a33d
fasihh <fhk.tcs@gmail.com>
2024-05-05 08:04:40 +0500

    yes

--- Assignments/owo.py
+++ Assignments/owo.py
@@ -3,13 +3,13 @@ import os
 std_id = "23K0018"
 files = [file for file in os.listdir() if file != os.path.basename(__file__)]
 
-def handle_file(file):
-	new_lines = []
-	with open(file) as cpp_file:
-		lines = cpp_file.readlines()
-		new_lines = [f"{line}\tcout << \"{std_id}\" << endl;\n" if line == "int main() {\n" else line for line in lines]
-	with open(file, "w") as cpp_file:
-		cpp_file.write("".join(new_lines))
+# def handle_file(file):
+# 	new_lines = []
+# 	with open(file) as cpp_file:
+# 		lines = cpp_file.readlines()
+# 		new_lines = [f"{line}\tcout << \"{std_id}\" << endl;\n" if line == "int main() {\n" else line for line in lines]
+# 	with open(file, "w") as cpp_file:
+# 		cpp_file.write("".join(new_lines))
 
 
 for i, file in enumerate(files):
